9f16ff47c4800fe00e969ad662f1737c89847e95
ArrKuu77 <aungkungsan@gmail.com>
2022-02-07 23:37:25 +0630

    parfectSpaceTourismMenuBar

--- script.js
+++ script.js
@@ -35,8 +35,8 @@ const MenuData = [
           {
             "name": "Moon",
             "images": {
-              "png": "./assets/destination/image-moon.png",
-              "webp": "./assets/destination/image-moon.webp"
+              "png": "starter-code/assets/destination/image-moon.png",
+              "webp": "starter-code/assets/destination/image-moon.webp"
             },
             "description": "See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
             "distance": "384,400 km",
@@ -45,8 +45,8 @@ const MenuData = [
           {
             "name": "Mars",
             "images": {
-              "png": "./assets/destination/image-mars.png",
-              "webp": "./assets/destination/image-mars.webp"
+              "png": "starter-code/assets/destination/image-mars.png",
+              "webp": "starter-code/assets/destination/image-mars.webp"
             },
             "description": "Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!",
             "distance": "225 mil. km",
@@ -55,8 +55,8 @@ const MenuData = [
           {
             "name": "Europa",
             "images": {
-              "png": "./assets/destination/image-europa.png",
-              "webp": "./assets/destination/image-europa.webp"
+              "png": "starter-code/assets/destination/image-europa.png",
+              "webp": "starter-code/assets/destination/image-europa.webp"
             },
             "description": "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.",
             "distance": "628 mil. km",
@@ -65,8 +65,8 @@ const MenuData = [
           {
             "name": "Titan",
             "images": {
-              "png": "./assets/destination/image-titan.png",
-              "webp": "./assets/destination/image-titan.webp"
+              "png": "starter-code/assets/destination/image-titan.png",
+              "webp": "starter-code/assets/destination/image-titan.webp"
             },
             "description": "The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.",
             "distance": "1.6 bil. km",
@@ -77,8 +77,8 @@ const MenuData = [
           {
             "name": "Douglas Hurley",
             "images": {
-              "png": "./assets/crew/image-douglas-hurley.png",
-              "webp": "./assets/crew/image-douglas-hurley.webp"
+              "png": "starter-code/assets/crew/image-douglas-hurley.png",
+              "webp": "starter-code/assets/crew/image-douglas-hurley.webp"
             },
             "role": "Commander",
             "bio": "Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2."
@@ -86,8 +86,8 @@ const MenuData = [
           {
             "name": "Mark Shuttleworth",
             "images": {
-              "png": "./assets/crew/image-mark-shuttleworth.png",
-              "webp": "./assets/crew/image-mark-shuttleworth.webp"
+              "png": "starter-code/assets/crew/image-mark-shuttleworth.png",
+              "webp": "starter-code/assets/crew/image-mark-shuttleworth.webp"
             },
             "role": "Mission Specialist",
             "bio": "Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist."
@@ -95,8 +95,8 @@ const MenuData = [
           {
             "name": "Victor Glover",
             "images": {
-              "png": "./assets/crew/image-victor-glover.png",
-              "webp": "./assets/crew/image-victor-glover.webp"
+              "png": "starter-code/assets/crew/image-victor-glover.png",
+              "webp": "starter-code/assets/crew/image-victor-glover.webp"
             },
             "role": "Pilot",
             "bio": "Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer."
@@ -104,8 +104,8 @@ const MenuData = [
           {
             "name": "Anousheh Ansari",
             "images": {
-              "png": "./assets/crew/image-anousheh-ansari.png",
-              "webp": "./assets/crew/image-anousheh-ansari.webp"
+              "png": "starter-code/assets/crew/image-anousheh-ansari.png",
+              "webp": "starter-code/assets/crew/image-anousheh-ansari.webp"
             },
             "role": "Flight Engineer",
             "bio": "Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space."
@@ -115,24 +115,24 @@ const MenuData = [
           {
             "name": "Launch vehicle",
             "images": {
-              "portrait": "./assets/technology/image-launch-vehicle-portrait.jpg",
-              "landscape": "./assets/technology/image-launch-vehicle-landscape.jpg"
+              "portrait": "starter-code/assets/technology/image-launch-vehicle-portrait.jpg",
+              "landscape": "starter-code/assets/technology/image-launch-vehicle-landscape.jpg"
             },
             "description": "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!"
           },
           {
             "name": "Spaceport",
             "images": {
-              "portrait": "./assets/technology/image-spaceport-portrait.jpg",
-              "landscape": "./assets/technology/image-spaceport-landscape.jpg"
+              "portrait": "starter-code/assets/technology/image-spaceport-portrait.jpg",
+              "landscape": "starter-code/assets/technology/image-spaceport-landscape.jpg"
             },
             "description": "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch."
           },
           {
             "name": "Space capsule",
             "images": {
-              "portrait": "./assets/technology/image-space-capsule-portrait.jpg",
-              "landscape": "./assets/technology/image-space-capsule-landscape.jpg"
+              "portrait": "starter-code/assets/technology/image-space-capsule-portrait.jpg",
+              "landscape": "starter-code/assets/technology/image-space-capsule-landscape.jpg"
             },
             "description": "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained."
           }
@@ -147,7 +147,7 @@ setTimeout(() => {
 },500);
 
 const HomeMenuDisplayFunction = () => {
-  Mother.style.backgroundImage = "url(./assets/home/background-home-desktop.jpg)";
+  Mother.style.backgroundImage = "url(starter-code/assets/home/background-home-desktop.jpg)";
     bodyContainer.innerHTML = "";
 
     const LeftContainer = document.createElement("div");
@@ -194,10 +194,10 @@ HomeMenu.addEventListener("click",() => {
 });
 HomeMobile.addEventListener("click",() => {
   HomeMenuDisplayFunction();
-  Mother.style.backgroundImage = "url(./assets/home/background-home-mobile.jpg)";
+  Mother.style.backgroundImage = "url(starter-code/assets/home/background-home-mobile.jpg)";
 })
 const DestinationDisplayFunction = () => {
-  Mother.style.backgroundImage = "url(./assets/destination/background-destination-desktop.jpg)"
+  Mother.style.backgroundImage = "url(starter-code/assets/destination/background-destination-desktop.jpg)"
   bodyContainer.innerHTML="";
   const LeftContainerMother = document.createElement("div");
   LeftContainerMother.classList.add("LeftContainerMother");
@@ -279,7 +279,7 @@ const DestinationDisplayFunction = () => {
   RightHeadText.append(" 01 Pick your destination")
 
   const RightImg = document.createElement("img");
-  RightImg.src= "./assets/destination/image-moon.webp";
+  RightImg.src= "starter-code/assets/destination/image-moon.webp";
   RightImg.classList.add("RightImg")
   const SmallMenuDisplay = (index) =>{
     LeftHeadText.innerHTML="";
@@ -307,12 +307,12 @@ DestinationMenu.addEventListener("click",() => {
 });
 DestinationMobile.addEventListener("click",() => {
   DestinationDisplayFunction();
-  Mother.style.backgroundImage = "url(./assets/destination/background-destination-mobile.jpg))"
+  Mother.style.backgroundImage = "url(starter-code/assets/destination/background-destination-mobile.jpg))"
 })
 
 CrewMenu.addEventListener("click", () => {
   SliderFunction(CrewMenu);
-  Mother.style.backgroundImage = "url(./assets/crew/background-crew-desktop.jpg)";
+  Mother.style.backgroundImage = "url(starter-code/assets/crew/background-crew-desktop.jpg)";
   bodyContainer.innerHTML = "";
 
   const LeftContainerCrew = document.createElement("div");
@@ -392,7 +392,7 @@ CrewMenu.addEventListener("click", () => {
     
 })
 CrewMobile.addEventListener("click",() => {
-  Mother.style.backgroundImage = "url(./assets/crew/background-crew-mobile.jpg)";
+  Mother.style.backgroundImage = "url(starter-code/assets/crew/background-crew-mobile.jpg)";
   bodyContainer.innerHTML = "";
 
   const LeftContainerCrew = document.createElement("div");
@@ -544,7 +544,7 @@ setTimeout(() => {
 }
 TechnologyMenu.addEventListener("click", () => {
   SliderFunction(TechnologyMenu);
-  Mother.style.backgroundImage = " url(./assets/technology/background-technology-desktop.jpg)";
+  Mother.style.backgroundImage = " url(starter-code/assets/technology/background-technology-desktop.jpg)";
   bodyContainer.innerHTML = "";
 
   const LeftContainerTechnology = document.createElement("div");
@@ -619,7 +619,7 @@ setTimeout(() => {
 })
 TechnologyMobile.addEventListener("click",() => {
   TechnologyDisplayFunction();
-  Mother.style.backgroundImage = " url(./assets/technology/background-technology-mobile.jpg)"
+  Mother.style.backgroundImage = " url(starter-code/assets/technology/background-technology-mobile.jpg)"
 
 })
 
